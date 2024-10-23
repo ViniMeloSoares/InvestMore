9bec78ad175f34c13137294c202c01da5cab86ba
ViniMeloSoares <vinimelosoares@gmail.com>
2024-10-23 02:08:49 -0300

    Atualizações no calculo e layout

--- app/src/main/java/com/viniciusmelo/investcalculator/MainActivity.kt
+++ app/src/main/java/com/viniciusmelo/investcalculator/MainActivity.kt
@@ -2,12 +2,15 @@ package com.viniciusmelo.investcalculator
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import java.text.NumberFormat
+import java.util.Locale
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +19,15 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+
     val edt_AporteInicial = findViewById<TextInputEditText>(R.id.edt_APInicial)
     val edt_AporteMes = findViewById<TextInputEditText>(R.id.edt_MensalAporte)
     val edt_Periodo = findViewById<TextInputEditText>(R.id.edt_Meses)
     val edt_Juros = findViewById<TextInputEditText>(R.id.edt_juros)
     val btn_calcular = findViewById<Button>(R.id.btn_Calcular)
     val btn_limpar = findViewById<Button>(R.id.btn_Limpar)
+    val formatoReal = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
 
 
     btn_calcular.setOnClickListener {
@@ -45,8 +51,8 @@ class MainActivity : AppCompatActivity() {
       val JurosDecimal = Juros/100
 
       val resultado = aporteInicial*(1+JurosDecimal).pow(Periodo) + (aporteMes*((1+JurosDecimal).pow(Periodo)-1))/JurosDecimal
-
-       println (resultado)
+      val valorfinal = findViewById<TextView>(R.id.ValorTotal)
+      valorfinal.text = formatoReal.format(resultado)
 
       }
     }
